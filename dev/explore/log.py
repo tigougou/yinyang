32c6111d20621f9684b423bbc52bfa9871f9dcab
lfs <654442608@qq.com>
2016-12-05 23:53:54 +0800

    correct dm.py and update log.py

--- dev/explore/log.py
+++ dev/explore/log.py
@@ -1,4 +1,15 @@
 from util.dm import *
+import time
 
 def bind(simulater):
     WindowBind(simulater)
+def log():
+    # 根据博远雅裤子的图片的相对位移来点击进入游戏
+    find_pic_loop(r"log/jinruyouxi.bmp",click_en=1, offsetx=399, offsety=151, wait_delta=1, times=180)
+
+
+
+bind(2)
+while(True):
+    log()
+    time.sleep(5)
