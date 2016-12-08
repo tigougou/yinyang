31412c3b23c3563b9d39f73cfd946c77a1c9b538
tigooooo <654442608@qq.com>
2016-12-09 00:24:25 +0800

    correct

--- dev/explore/explore_function.py
+++ dev/explore/explore_function.py
@@ -133,7 +133,7 @@ def autoexplore(chapter,difficulty_mode = 0):
     ret = find_pic_loop("explore/baoxiang.bmp", click_en=0, sim=0.8, times=20, wait_delta=0.1)
     if ret != "":
         scene_chang_handle("explore/fightend_win_gift1.bmp", "explore/baoxiang.bmp", delaytime=0.01, sim=0.6,
-                           tryTimes=200)
+                           tryTimes=2000)
         scene_chang_handle("explore/fightend_win_giftopen.bmp", "explore/fightend_win_gift1.bmp", delaytime=0.01,
                            sim=0.6, tryTimes=2000)
         scene_chang_handle("explore/exploreEnterflag.bmp", "explore/fightend_win_giftopen.bmp", delaytime=0.1,
