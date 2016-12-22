bb087206961f04f6f7bfdb225101aa0e11f0e5af
tigooooo <654442608@qq.com>
2016-12-22 22:57:39 +0800

    update

--- dev/activity/activity_function.py
+++ dev/activity/activity_function.py
@@ -7,9 +7,9 @@ def activity_power_get():
         ret = find_pic_loop('activity/power_get.bmp',times= 100)
         if(ret == ""):
             return 0
-            time.sleep(10)
-            moveto(45,163)
-            left_click()
+        time.sleep(10)
+        moveto(45,163)
+        left_click()
     return 1
 if __name__ == '__main__':
     ret = find_pic_loop('activity/power_get_success.bmp',offsetx=79,offsety=158,times= 20)
